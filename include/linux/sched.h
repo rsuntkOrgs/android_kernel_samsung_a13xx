fcbe95725c89b47921b497501e52105c967bcb4d
rsuntk <rissu.ntk@gmail.com>
2025-07-19 00:07:51 +0000

    treewide: Track susfs-v1.5.9 changes [2]
    Tracked:
    kernel: Remove leftover kabi member
    
    Signed-off-by: rsuntk <rissu.ntk@gmail.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1411,9 +1411,6 @@ struct task_struct {
 	 * New fields for task_struct should be added above here, so that
 	 * they are included in the randomized portion of task_struct.
 	 */
-#ifdef CONFIG_KSU_SUSFS
-	u64 susfs_task_state;
-#endif
 	randomized_struct_fields_end
 
 	/* CPU-specific state of this task: */
