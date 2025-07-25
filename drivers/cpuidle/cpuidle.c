b5eba239cb18dc02c1edc5be46a66dd175438064
rsuntk <rissu.ntk@gmail.com>
2025-07-25 05:56:59 +0000

    drivers/cpuidle: Nuke debug snapshot
    Signed-off-by: rsuntk <rissu.ntk@gmail.com>

--- drivers/cpuidle/cpuidle.c
+++ drivers/cpuidle/cpuidle.c
@@ -274,7 +274,6 @@ int cpuidle_enter_state(struct cpuidle_device *dev, struct cpuidle_driver *drv,
 	sched_idle_set_state(target_state, index);
 
 	trace_cpu_idle(index, dev->cpu);
-	dbg_snapshot_cpuidle(drv->states[index].desc, index, 0, DSS_FLAG_IN);
 	time_start = ns_to_ktime(local_clock());
 
 	stop_critical_timings();
@@ -285,8 +284,6 @@ int cpuidle_enter_state(struct cpuidle_device *dev, struct cpuidle_driver *drv,
 
 	sched_clock_idle_wakeup_event();
 	time_end = ns_to_ktime(local_clock());
-	dbg_snapshot_cpuidle(drv->states[index].desc, entered_state,
-			(int)ktime_to_us(ktime_sub(time_end, time_start)), DSS_FLAG_OUT);
 	trace_cpu_idle(PWR_EVENT_EXIT, dev->cpu);
 
 	/* The cpu is no longer idle or about to enter idle. */
